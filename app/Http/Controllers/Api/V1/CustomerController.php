c5165a077f7dc0b3699cbd8c11b6d092bfddc23c
Farhan <faarhan7@gmail.com>
2022-10-25 16:39:01 +0700

    add filter to invoices

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -9,7 +9,7 @@ use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Requests\UpdateCustomerRequest;
-use App\Services\V1\CustomerQuery;
+use App\Filters\V1\CustomersFilter;
 
 class CustomerController extends Controller
 {
@@ -20,13 +20,14 @@ class CustomerController extends Controller
      */
     public function index(Request $request)
     {
-        $filter = new CustomerQuery();
+        $filter = new CustomersFilter();
         $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
 
         if(count($queryItems) == 0){
             return new CustomerCollection(Customer::paginate());
         }else{
-            return new CustomerCollection(Customer::where($queryItems)->paginate());
+            $customers = Customer::where($queryItems)->paginate();
+            return new CustomerCollection($customers->appends($request->query()));
         }
     }
 
